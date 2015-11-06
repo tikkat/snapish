bce8d45f87f52ded5d304a35a4116c763dd8e43d
Tobias Tikka <tobias.tikka@gmail.com>
2015-11-06 10:01:28 +0100

    Initial Commit

---
+++ Watchit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Watchit
+//
+//  Created by Tobias Tikka on 06/11/15.
+//  Copyright © 2015 Tobias Tikka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Watchit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Watchit
+//
+//  Created by Tobias Tikka on 06/11/15.
+//  Copyright © 2015 Tobias Tikka. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
